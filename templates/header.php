eb91a4e7b3e34a79fb6d2e474cff085a39bbb9fd
trocatudo95 <up201504779@B02AY0407.fe.up.pt>
2017-11-22 12:15:13 +0000

    moved form to header

--- templates/header.php
+++ templates/header.php
@@ -9,4 +9,18 @@
   <body>
     <header>
       <h1><a href="index.php">Quick Buddy</a></h1>
+
+      <div id="login">
+        <form action="action_login.php" method="post">
+        <fieldset>
+          <legend>Login</legend>
+          <p>Username</p>
+          <input type="text" name="username" required="required">
+          <p>Password</p>
+          <input type="password" name="password" required="required">
+        </fieldset>
+        <input type="submit" value="Login">
+      </form>
+      </div>
+      <a href="templates/register_form.php">Register</a>
     </header>
