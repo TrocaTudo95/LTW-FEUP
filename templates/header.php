ae0797fb0f0d7131ff2b88c15e4a4eb366ac4bd9
ineeve <renatoineeve@gmail.com>
2017-11-29 12:07:14 +0000

    added font awesome

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/layout.css">
     <script src="scripts/script.js" defer></script>
