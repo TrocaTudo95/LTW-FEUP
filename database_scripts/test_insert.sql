2efbb791ea021789bd97c03492b116946aa7ab2d
Pedro <pedro21fcp@gmail.com>
2017-11-29 13:01:54 +0000

    update database

---
+++ database_scripts/test_insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO users (id,username,password,email,apiKey) VALUES (1,"JoaoM","pila","p@gmail.com","asdf");
+
+INSERT INTO categories (id,title,userRef) VALUES (1,"SuperMarket",1);
+
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (1,"Gordon","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (2,"Moreno","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (3,"Cochran","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (4,"Cobb","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (5,"Morales","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (6,"Fowler","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (7,"Ferguson","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (8,"Navarro","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (9,"Dale","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (10,"Finch","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (11,"Carver","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (12,"Carrillo","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (13,"Tanner","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (14,"Houston","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (15,"Douglas","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (16,"Massey","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (17,"Foley","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (18,"Conley","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (19,"Harrington","#ff0000",1,1);
+INSERT INTO projects (id,name,color,creator,categoryRef) VALUES (20,"Rutledge","#ff0000",1,1);
