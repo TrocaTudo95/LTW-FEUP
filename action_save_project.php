ff89cbab52b226b7077d1433e26fc4d35c2c8f38
trocatudo95 <pedromachado_95@sapo.pt>
2017-12-10 22:13:24 +0000

    merda

--- action_save_project.php
+++ action_save_project.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();  
+    session_start();
     if (!isset($_SESSION['username'])) die('No username');
     if (!isset($_GET['title'])) die('No title');
     if (!isset($_GET['category'])) die('No category');
