98ced8439ea09d733c8dcc4926ccbd5e99686afb
trocatudo95 <pedromachado_95@sapo.pt>
2017-12-12 18:18:41 +0000

    profile done

---
+++ action_change_password.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+if(!isset($_SESSION['username'])) die('Login required');
+if(!isset($_GET['pass'])) die('pass not set');
+include_once("database/connection.php");
+include_once("database/users.php");
+
+  $user_id = getUserId($dbh,$_SESSION['username']);
+  $pass =$_GET['pass'];
+  $stmt = $dbh->prepare("UPDATE users SET password=? WHERE id=?");
+  $stmt->execute(array(hash('sha256',$pass),$user_id));
+  header('Location: user_profile.php');
+?>
--- user_profile.php
+++ user_profile.php
@@ -8,11 +8,8 @@ $userId = getUserId($dbh,$_SESSION['username']);
 $username =$_SESSION['username'];
 $imageName= getImageFromUser($dbh,$userId);
 ?>
-<html>
 <link rel="stylesheet" href="css/profile_style.css">
 <div class="container">
-    <div class="row profile">
-		<div class="col-md-3">
 			<div class="profile-sidebar">
 				<!-- SIDEBAR USERPIC -->
 				<div class="profile-userpic">
@@ -23,31 +20,30 @@ $imageName= getImageFromUser($dbh,$userId);
 				<!-- SIDEBAR USER TITLE -->
 				<div class="profile-usertitle">
             <?php echo $username;?>
-					<div class="profile-usertitle-name">
-					</div>
 				</div>
 
+        <form class="centered_form"  action="action_change_password.php">
+        Change Password:
+        <input type="password" name="pass" value="">
+        <input type="submit" class="profile_button" value="Change">
+        </form>
         <nav>
-      <form action="upload_image.php" method="post" enctype="multipart/form-data">
-        <label>Choose a file to upload:
+      <form class="centered_form" action="upload_image.php" method="post" enctype="multipart/form-data">
+        <label >Change profile picture:
         <input type="text" name="username" style="display: none" value=<?=$username?>>
       </label>
         <input type="file" name="image" required>
-        <input type="submit" value="Upload">
+        <input type="submit" class="profile_button" value="Upload">
       </form>
     </nav>
+
+
 				<!-- END SIDEBAR BUTTONS -->
 				<!-- SIDEBAR MENU -->
 
 				<!-- END MENU -->
 			</div>
-		</div>
-		<div class="col-md-9">
-            <div class="profile-content">
-			   Some user related content goes here...
-            </div>
-		</div>
-	</div>
+
 </div>
-</html>
+
 <?php include_once ('templates/footer.php'); ?>
