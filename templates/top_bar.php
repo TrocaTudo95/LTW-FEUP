836f7b899b481d810f5ba2e8a8326adb6f64a9e4
trocatudo95 <pedromachado_95@sapo.pt>
2017-12-12 22:43:02 +0000

    added some css

--- templates/top_bar.php
+++ templates/top_bar.php
@@ -1,3 +1,11 @@
 <section id="top_bar">
     <span id="new_project" onClick="new_project_click()">New Project</span>
+    <form id="search_form">
+      <input type="search" id="searchfield" placeholder="Search" oninput="updateProjects()">
+    </form>
+    <select id="filter">
+      <option value="name">Name</option>
+      <option value="category">Category</option>
+      <option value="task">Task</option>
+    </select>
 </section>
