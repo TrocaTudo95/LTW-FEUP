00068b7b9e3819178e5ec952425936a0470bdd04
Pedro <pedro21fcp@gmail.com>
2017-12-13 10:28:21 +0000

    update

--- user_profile.php
+++ user_profile.php
@@ -4,9 +4,6 @@ include_once('database/connection.php');
 include_once('database/users.php');
 include_once('templates/header.php');
 if(!isset($_SESSION['username'])) die('username not set');
-if ($_SESSION['csrf'] !== $_POST['csrf']) {
-  die('-3');
-}
 if (isset($_SESSION['username']) && !isset($_GET['username'])){
   include_once('templates/edit_profile.php');
 }
